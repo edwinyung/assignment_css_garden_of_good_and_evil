84afb7c1ae85ee99e0689344c62908948531e309
edwinyung <edwinyung@github.com>
2017-11-20 12:41:09 -0500

    initial

---
+++ app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let fs = require ('fs');
+let http = require ('http');
+let express = require('express')();
+
+// Set up form body parsing
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set up cookies
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+// ----------------------------------------
+// Sessions/Cookies
+// ----------------------------------------
+var cookieSession = require('cookie-session');
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['asdf1234567890qwer']
+}));
+
+// Set up handlebars
+const exphbs = require("express-handlebars");
+const helpers = require('./helpers');
+app.engine("handlebars", exphbs({
+  defaultLayout: "main",
+  helpers: helpers.registered,
+  partialsDir: 'views/'
+}));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  console.log("Hello world!");
+  res.render("index");
+});
+
+app.listen(3000, 'localhost', () => {
+  console.log("Listening!");
+});
